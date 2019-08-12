f94265971e5c86f9b782ce740969bfc64757cca3
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-12 13:54:43 -0600

    Wrapped components in CartContext, refactored ShoppingCart for context API

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Context
 import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -20,22 +21,24 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{products, addItem}}>
-			<div className="App">
-				<Navigation cart={cart} />
-	
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}/>
-					)}
-				/>
-	
-				<Route
-					path="/cart"
-					component={ShoppingCart}
-				/>
-			</div>
+			<CartContext.Provider >
+				<div className="App">
+					<Navigation cart={cart} />
+		
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}/>
+						)}
+					/>
+		
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
