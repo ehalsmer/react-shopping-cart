e796d54f6e2e482a23944744447e992f3ae2322f
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-12 13:46:34 -0600

    Refactoring from props to get data from context API

--- src/App.js
+++ src/App.js
@@ -27,17 +27,13 @@ function App() {
 				<Route
 					exact
 					path="/"
-					render={() => (
-						<Products
-							products={products}
-							addItem={addItem}
-						/>
+					component={Products}/>
 					)}
 				/>
 	
 				<Route
 					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
+					component={ShoppingCart}
 				/>
 			</div>
 		</ProductContext.Provider>
